cd1aab0399112f29504a530cff3ddb372e5cecdb
rahulk0127@gmail.com <rahulk4@illinois.edu>
2019-04-09 08:52:28 -0500

    Added functions to audioFunctions.py that return dictionaries keyed appropriately

--- audioFunctions.py
+++ audioFunctions.py
@@ -28,6 +28,7 @@ def trackSplit(mSong, lTimestamps, lTrackNames, sArtist, sAlbum):
         dTracks[name] = tempSong.export(name + ".mp3", format="mp3", tags={"track": index, "artist": sArtist, "album" : sAlbum})
     return dTracks
 
+
 #Crops an audio track passed to it, does the same stuff as splitter otherwise
 #pass AUdioSegment mp3 file type, and integer times in seconds.
 #exports dictionary with key as song name.
@@ -41,3 +42,40 @@ def trackCrop(mSong, iStartTime, iEndTime, sTitle, sArtist, sAlbum):
 def trackSpeedUp(mSong, iFactor, sTitle, sArtist, sAlbum):
     tempSong = speedup(mSong, float(iFactor))
     return { sTitle : tempSong.export(sTitle + ".mp3", format="mp3", tags={"artist" : sArtist, "album" : sAlbum}) }
+
+
+#gets a catchy hook composed from the first seven seconds of a
+#song and the last five seconds of a song, mixed together
+#pass AudioSegment (mp3 file type), and its title, artist, and album
+#exports dictionary with key as song name
+def getCatchyHook(mSong, sTitle, sArtist, sAlbum):
+	first_7 = tempSong[:7000]
+	last_5 = tempSong[-5000:]
+	beginning = first_7 + 6;
+	end = last_5 - 3;
+	cropped = beginning + end
+	faded = beginning.append(end, crossfade=1500)
+	faded_x2 = faded * 2
+	catchy_hook = faded_x2.fade_in(2000).fade_out(3000)
+	return { sTitle : catchy_hook.export(sTitle + ".mp3", format="mp3", tags={"artist" : sArtist, "album" : sAlbum})  }
+
+
+#gets an audio clip of silence for a specified duration
+#pass an integer value of milliseconds of silence to get
+#exports dicitonary with key as iDuration_second_silence
+def getSilence(iDuration):
+	silence = AudioSegment.silent(duration=iDuration)
+	return { "" + iDuration + "_second_silence" : silence.export("" + iDuration + "_second_silence.mp3", format="mp3") }
+
+
+#combines a list of AudioSegments (file type mp3) into one audio file called mixtape.mp3
+#pass a list of AudioSegments (file type mp3)
+#exports dictionary with key as "mixtape"
+def combineClips(*argv):
+	clips = []
+	for arg in argv:
+		clips.append(arg)
+	combined = AudioSegment.empty()
+	for clip in clips:
+		combined += AudioSegment.from_mp3(clip)
+	return { "mixtape" : combined.export("mixtape.mp3", format="mp3") }
